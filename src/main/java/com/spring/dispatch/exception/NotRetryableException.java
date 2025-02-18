9ca2a06a7ade578e7dc372abfd7152ffcb032135
bungearntun <love2554f@hotmail.com>
2025-02-18 13:28:22 +0700

    added : exception hanler classes

---
+++ src/main/java/com/spring/dispatch/exception/NotRetryableException.java
@@ -0,0 +1,8 @@
+package com.spring.dispatch.exception;
+
+public class NotRetryableException extends RuntimeException {
+
+    public NotRetryableException(Exception exception) {
+        super(exception);
+    }
+}
---
+++ src/main/java/com/spring/dispatch/exception/RetryableException.java
@@ -0,0 +1,12 @@
+package com.spring.dispatch.exception;
+
+public class RetryableException extends RuntimeException {
+
+    public RetryableException(String message) {
+        super(message);
+    }
+
+    public RetryableException(Exception exception) {
+        super(exception);
+    }
+}
