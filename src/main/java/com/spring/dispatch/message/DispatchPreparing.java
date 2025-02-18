a29673c8ac49ba231b1d6bb588c55966ffba40ee
bungearntun <love2554f@hotmail.com>
2025-02-18 08:13:21 +0700

    added : data models

---
+++ src/main/java/com/spring/dispatch/message/DispatchPreparing.java
@@ -0,0 +1,16 @@
+package com.spring.dispatch.message;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DispatchPreparing {
+    UUID orderId;
+}
